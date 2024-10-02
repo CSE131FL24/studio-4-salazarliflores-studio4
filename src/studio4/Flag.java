a40e9016a9da50fd677852cc77c6d34625f355ca
holly <holly@DESKTOP-DIGE2O5.dhcp.wustl.edu>
2024-10-02 17:19:28 -0500

    finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,20 @@
 package studio4;
 
 import java.awt.Color;
+import edu.princeton.cs.introcs.StdDraw;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setCanvasSize(512, 512);
+		StdDraw.clear(StdDraw. GREEN);
+		double[] x = { 0.4, 0.5, 0.5, 0.6 };
+		double[] y = { 0.5, 0.6, 0.4, 0.5 };
+		StdDraw.filledPolygon(x, y);
+		double[] rotatedX = {0.5, 0.6, 0.4, 0.5};
+		double[] rotatedY = {0.6, 0.5, 0.5, 0.4};
+		StdDraw.filledPolygon(rotatedX, rotatedY);
 	}
+	
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
 import java.io.File;
+
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -18,7 +20,21 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		Scanner scan = new Scanner(f); //making Scanner with a File
+		String shapeType = scan.next();
+		int redComponent = scan.nextInt();
+		int greenComponent = scan.nextInt();
+		int blueComponent = scan.nextInt();
+		boolean isFilled= scan.nextBoolean();
+		double parameterOne = scan.nextDouble();
+		double parameterTwo = scan.nextDouble();
+		double parameterThree = scan.nextDouble();
+		double parameterFour = scan.nextDouble();
+
+		if( shapeType.equals("rectangle")){
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
 	}
-}
+	}
